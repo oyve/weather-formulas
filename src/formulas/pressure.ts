aeafbd106b0a51adb8d16045bf11300988c326bf
oyve <oyve-github@outlook.com>
2025-03-26 12:23:33 +0100

    adjust to sea level

--- src/formulas/pressure.ts
+++ src/formulas/pressure.ts
@@ -1,13 +1,14 @@
+import c, {AtmospericConstants} from '../constants'
+
 /**
  * Calculate Pressure Altitude
  * @param {number} pressure Pressure in Pa (Pascal)
  * @returns {number} Pressure Altitude in meters (m)
  */
 function pressureAltitude(pressure: number): number {
-    const standardPressure = 101325; // Standard atmospheric pressure in Pa
-    return (1 - Math.pow(pressure / standardPressure, 0.190284)) * 145366.45 * 0.3048;
+    return (1 - Math.pow(pressure / c.STANDARD_MEAN_PRESSURE_SEA_LEVEL, 0.190284)) * 145366.45 * 0.3048;
 }
-
+    
 /**
  * Calculate Density Altitude
  * @param {number} pressureAltitude Pressure Altitude in meters (m)
@@ -15,11 +16,42 @@ function pressureAltitude(pressure: number): number {
  * @returns {number} Density Altitude in meters (m)
  */
 function densityAltitude(pressureAltitude: number, temperature: number): number {
-    const standardTemperature = 288.15; // Standard temperature in Kelvin
-    return pressureAltitude + (120 * (temperature - standardTemperature));
+    return pressureAltitude + (120 * (temperature - c.STANDARD_MEAN_TEMPERATURE_KELVIN));
+}
+
+/**
+ * 
+ * @description This function reduces observerd pressure to sea level using a faster, simpler calculation for standard sea level mean temperature (15C), but will be off with other temperatures.
+ * @param pressureObserved Observed pressure
+ * @param altitude Altitude of observered pressure
+ * @param temperatureAtSeaLevel Temperature at sea level. Default standard mean temperature: 15 Celcius
+ * @returns {number} Reduced pressure to sea level in Pascals with two decimal precision
+ */
+function adjustPressureToSeaLevelSimple(pressureObserved: number, altitude: number, temperatureAtSeaLevel: number = c.STANDARD_MEAN_TEMPERATURE_KELVIN): number {
+	let pressureSeaLevel = pressureObserved * Math.pow(1 - ((0.0065 * altitude) / (temperatureAtSeaLevel + 0.0065 * altitude)), -5.257);
+
+	return Number(pressureSeaLevel.toFixed(2));
+}
+
+/**
+ * 
+ * @description This function reduces observed pressure to sea level with more accuracy for different temperatures, and with option to replace Earth's standard constants.
+ * @param pressureObserved Observed pressure
+ * @param altitude Altitude of observered pressure
+ * @param temperatureAtSeaLevel Temperature at sea level in Kelvin. Default standard mean temperature: 15 Celcius
+ * @returns {number} Reduced pressure to sea level in Pascals with two decimal precision
+ */
+function adjustPressureToSeaLevelAdvanced(pressureObserved: number, altitude: number, temperatureAtSeaLevel: number = c.STANDARD_MEAN_TEMPERATURE_KELVIN, constants: AtmospericConstants = c.DEFAULT_ATMOSPHERIC_CONSTANTS): number {
+    let pressureSeaLevel = pressureObserved * Math.pow(
+        (1 - (constants.lapseRate * altitude) / temperatureAtSeaLevel),
+        (constants.gravity * constants.molarMass) / (constants.gasConstant * constants.lapseRate));
+
+    return Number(pressureSeaLevel.toFixed(2));
 }
 
 export default {
     pressureAltitude,
-    densityAltitude
+    densityAltitude,
+    adjustPressureToSeaLevelSimple,
+    adjustPressureToSeaLevelAdvanced
 };
--- tests/formulas/pressure.test.ts
+++ tests/formulas/pressure.test.ts
@@ -1,13 +1,13 @@
 import pressure from "../../src/formulas/pressure";
-
+import temperature from "../../src/formulas/temperature"
 
 describe('Pressure Tests', function() {
     describe('Pressure Altitude', function() {
         it('should calculate pressure altitude correctly', function() {
             const pressurePa = 90000; // Pa
-            const expectedAltitude = 988.0928355703854; // m
+            const expected = 988.0928355703854; // m
             const result = pressure.pressureAltitude(pressurePa);
-            expect(result).toEqual(expectedAltitude);
+            expect(result).toEqual(expected);
         });
     });
 
@@ -15,9 +15,51 @@ describe('Pressure Tests', function() {
         it('should calculate density altitude correctly', function() {
             const pressureAlt = 1112.87; // m
             const kelvin = 293.15; // K
-            const expectedDensityAlt = 1712.87; // m
+            const expected = 1712.87; // m
             const result = pressure.densityAltitude(pressureAlt, kelvin);
-            expect(result).toEqual(expectedDensityAlt);
+            expect(result).toEqual(expected);
+        });
+    });
+
+    describe('Adjust Pressure To Sea Level Simple', function() {
+        it('should calculate pressure correctly', function() {
+            const input = { pressure: 98000, altitude: 100 };
+            const expected = 99167.73;
+            const result = pressure.adjustPressureToSeaLevelSimple(input.pressure, input.altitude);
+            expect(result).toEqual(expected);
+        });
+        it('should calculate pressure correctly with temperature', function() {
+            const input = { pressure: 98000, altitude: 100, temperature: temperature.celciusToKelvin(30) };
+            const expected = 99109.69;
+            const result = pressure.adjustPressureToSeaLevelSimple(input.pressure, input.altitude, input.temperature);
+            expect(result).toEqual(expected);
+        });
+        it('should calculate pressure correctly with only temperature', function() {
+            const input = { pressure: 98000, altitude: 0, temperature: temperature.celciusToKelvin(30) };
+            const expected = 98000;
+            const result = pressure.adjustPressureToSeaLevelSimple(input.pressure, input.altitude, input.temperature);
+            expect(result).toEqual(expected);
+        });
+    });
+
+    describe('Adjust Pressure To Sea Level Advanced', function() {
+        it('should calculate pressure correctly', function() {
+            const input = { pressure: 98000, altitude: 100 };
+            const expected = 96843.69;
+            const result = pressure.adjustPressureToSeaLevelAdvanced(input.pressure, input.altitude);
+            expect(result).toEqual(expected);
+        });
+        it('should calculate pressure correctly with temperature', function() {
+            const input = { pressure: 98000, altitude: 100, temperature: temperature.celciusToKelvin(30) };
+            const expected = 96900.65;
+            const result = pressure.adjustPressureToSeaLevelAdvanced(input.pressure, input.altitude, input.temperature);
+            expect(result).toEqual(expected);
+        });
+        it('should calculate pressure correctly with only temperature', function() {
+            const input = { pressure: 98000, altitude: 0, temperature: temperature.celciusToKelvin(30) };
+            const expected = 98000;
+            const result = pressure.adjustPressureToSeaLevelAdvanced(input.pressure, input.altitude, input.temperature);
+            expect(result).toEqual(expected);
         });
     });
 });
