a5bc6058cfa6bc632f54d26f20821dcf930230b0
oyve <oyve-github@outlook.com>
2025-04-04 17:26:48 +0200

    add airdensity

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import humidity from "./formulas/humidity";
 import temperature from "./formulas/temperature";
 import pressure from "./formulas/pressure";
 import wind from "./formulas/wind";
+import airDensity from "./formulas/airDensity";
 
 export {
     humidity,
