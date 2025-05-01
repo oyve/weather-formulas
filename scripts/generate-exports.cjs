5bdc01c0be1b66dec70654916030285bec3e707c
oyve <oyve-github@outlook.com>
2025-05-01 09:46:36 +0200

    include scales in script

--- scripts/generate-exports.cjs
+++ scripts/generate-exports.cjs
@@ -1,34 +1,45 @@
 const fs = require('fs');
 const path = require('path');
 
-// Define paths
-const cjsDir = path.resolve(__dirname, '../dist/cjs/formulas');
-const esmDir = path.resolve(__dirname, '../dist/esm/formulas');
-const pkgPath = path.resolve(__dirname, '../package.json');
+function generateExports(directory) {
+  // Define paths
+  const cjsDir = path.resolve(__dirname, `../dist/cjs/${directory}`);
+  const esmDir = path.resolve(__dirname, `../dist/esm/${directory}`);
 
-// Get all JavaScript files in the ESM directory
-const files = fs.readdirSync(esmDir).filter(file => file.endsWith('.js'));
+  // Get all JavaScript files in the ESM directory
+  const files = fs.readdirSync(esmDir).filter(file => file.endsWith('.js'));
 
-// Initialize the exports field with the default index file
-const exportsField = {
-  '.': {
-    require: './dist/cjs/index.cjs',
-    import: './dist/esm/index.js',
-  },
-};
-
-// Add entries for other files dynamically
-files.forEach(file => {
-  const name = `./${path.basename(file, '.js')}`;
-  exportsField[name] = {
-    require: `./dist/cjs/formulas/${file.replace('.js', '.cjs')}`,
-    import: `./dist/esm/formulas/${file}`,
+  // Initialize the exports field with the default index file
+  const exportsField = {
+    '.': {
+      require: './dist/cjs/index.cjs',
+      import: './dist/esm/index.js',
+    },
   };
-});
 
-// Read, update, and write the package.json file
+  // Add entries for other files dynamically
+  files.forEach(file => {
+    const name = `./${path.basename(file, '.js')}`;
+    exportsField[name] = {
+      require: `./dist/cjs/${directory}/${file.replace('.js', '.cjs')}`,
+      import: `./dist/esm/${directory}/${file}`,
+    };
+  });
+
+  // Read, update, and write the package.json file
+
+  console.log(`Generated exports for directory /${directory}`);
+  return exportsField;
+}
+
+let exportFormulas = generateExports('formulas');
+let scalesFormulas = generateExports('scales');
+
+const exportsField = {...exportFormulas, ...scalesFormulas };
+
+const pkgPath = path.resolve(__dirname, '../package.json');
 const pkg = JSON.parse(fs.readFileSync(pkgPath, 'utf-8'));
 pkg.exports = exportsField;
 
 fs.writeFileSync(pkgPath, JSON.stringify(pkg, null, 2));
-console.log('Updated exports in package.json');
+console.log('Finished exports in package.json');
