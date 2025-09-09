59db2fa62a63c1688c703db5690a2b3bc9a05cc0
Oyve <oyve-github@outlook.com>
2025-09-09 10:01:59 +0200

    add tests

--- src/scales/saffirSimpson.ts
+++ src/scales/saffirSimpson.ts
@@ -9,7 +9,7 @@ export interface SaffirSimpsonScale  {
 export function getSaffirSimpsonScaleByWindSpeed(windSpeed: number): SaffirSimpsonScale | null {
   if(windSpeed < 0) throw new Error('Wind speed cannot be negative.');
   windSpeed = Math.round(windSpeed); //to nearest integer
-    
+
   const categories = [
     { min: 33, max: 42, category: 1 },
     { min: 43, max: 49, category: 2 },
@@ -17,6 +17,8 @@ export function getSaffirSimpsonScaleByWindSpeed(windSpeed: number): SaffirSimps
     { min: 59, max: 69, category: 4 },
     { min: 70, max: Infinity, category: 5 }
   ];
+
+  if(windSpeed < categories[0].min) return null;
 
   return categories.find(cat => windSpeed >= cat.min && windSpeed <= cat.max) || null;
 }
---
+++ tests/scales/saffirSimpson.test.ts
@@ -0,0 +1,35 @@
+import * as saffirSimpsonFormulas from '../../src/scales/saffirSimpson';
+
+describe('Saffir-Simpson Hurricane Wind Scale', () => {
+    it('should return the correct category for a given wind speed', () => {
+        // Arrange
+        const testCases = [
+            { windSpeed: 30, expectedCategory: null },
+            { windSpeed: 33, expectedCategory: 1 },
+            { windSpeed: 42, expectedCategory: 1 },
+            { windSpeed: 43, expectedCategory: 2 },
+            { windSpeed: 49, expectedCategory: 2 },
+            { windSpeed: 50, expectedCategory: 3 },
+            { windSpeed: 58, expectedCategory: 3 },
+            { windSpeed: 59, expectedCategory: 4 },
+            { windSpeed: 69, expectedCategory: 4 },
+            { windSpeed: 70, expectedCategory: 5 },
+            { windSpeed: 80, expectedCategory: 5 },
+            { windSpeed: 150, expectedCategory: 5 }
+        ];
+
+        // Act & Assert
+        testCases.forEach(({ windSpeed, expectedCategory }) => {
+            const actual = saffirSimpsonFormulas.getSaffirSimpsonScaleByWindSpeed(windSpeed);
+            if (actual === null) {
+                expect(actual).toBeNull();
+            } else {
+                expect(actual.category).toBe(expectedCategory);
+            }
+        });
+    });
+    
+    it('should throw error if negative wind speed', () => {
+        expect(() => saffirSimpsonFormulas.getSaffirSimpsonScaleByWindSpeed(-10)).toThrow('Wind speed cannot be negative.');
+    });
+});
