7b68d45d6e5c95f874240dbf6d7702a0da3aaef2
Oyve <oyve-github@outlook.com>
2025-09-11 08:55:25 +0200

    remove obsolete arguments

--- src/formulas/airDensity.ts
+++ src/formulas/airDensity.ts
@@ -6,8 +6,6 @@ import * as c from '../constants';
  * Supports both dry air and moist air calculations.
  * @param {number} pressure - Atmospheric pressure in Pascals (Pa).
  * @param {number} temperature - Temperature in Kelvin (K).
- * @param {number} humidity - Relative Humidity in percentage (%).
- * @param {boolean} [isDryAir=false] - Whether to calculate for dry air (ignores humidity).
  * @param {number} [gasConstant=287.05] - Specific gas constant for dry air (J/(kg·K)).
  * @returns {number} - Air density in kilograms per cubic meter (kg/m³).
  */
